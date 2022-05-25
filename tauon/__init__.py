7c3c9633e270018916b4dd7934d067b2d532d968
ice777 <ikkefornoyd777@gmail.com>
2022-05-25 18:26:46 +0300

    zort

--- tauon/__init__.py
+++ tauon/__init__.py
@@ -1,6 +1,8 @@
 from tauon.logger import logging
 from tauon.core.client import Client
 from tauon.config import API_HASH, API_ID, SESSION_STRING
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import requests
 
 HELP = {}
 
@@ -13,3 +15,12 @@ tauon = Client(
     workers=24,
     sleep_threshold=60,
 )
+
+
+def api_kaldir():
+    requests.get("https://apiice.herokuapp.com/")
+
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(api_kaldir, "interval", minutes=29)
+scheduler.start()
